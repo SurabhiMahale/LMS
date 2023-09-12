78e03f9f80b575baa8ff78fe1cc0aa0b94a6a5ef
Surabhi Mahale <surabhimahale12@gmail.com>
2023-09-12 17:30:36 +0530

    MongoDB connection

--- server/server.ts
+++ server/server.ts
@@ -1,7 +1,9 @@
 import { app } from "./app";
+import connectDB from "./utlis/db";
 require("dotenv").config();
 
 // create server
 app.listen(process.env.PORT, () => {
   console.log(`Server is connected to port ${process.env.PORT}`);  //Backticks
+  connectDB();
 });
